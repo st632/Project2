9c1bc6a70ee22519ad9a747324384efadaf2f710
Shubhang Sumnesh Taparia <st632@afsconnect1.njit.edu>
2017-12-06 15:09:09 -0500

    adding files

---
+++ pages/create_task.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!--<?php
+//this is how you print something  $data contains the record that was selected on the table.
+//print_r($data);
+//$id= $data->id;
+echo '<br>';
+?>-->
+ID:<input type="text" name="id" value=""><br>
+Owner Email:<input type="text" name="owneremail" value=""><br>
+Owner ID:<input type="text" name="ownerid" value=""><br>
+Created Date:<input type="text" name="createddate" value=""><br>
+Due Date:<input type="text" name="duedate" value=""><br>
+Message:<input type="text" name="message" value=""><br>
+Is Done:<input type="text" name="isdone" value=""><br>
+
+
+<form action="index.php?page=tasks&action=store" method="post" id="form1">
+    <button type="submit" form="form1" value="save">Create</button>
+</form>
+
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
