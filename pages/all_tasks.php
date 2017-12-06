2c311ae5f51930fd78bc9c8feed4749319cb097a
Shubhang Sumnesh Taparia <st632@afsconnect1.njit.edu>
2017-12-06 15:07:46 -0500

    adding files

---
+++ pages/all_tasks.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something
+$text='';
+$text.='<centre>';
+$text.='<h1><a href="index.php?page=tasks&action=create">Create New Task</a></h1>';
+$text.='</center>';
+echo $text;
+print utility\htmlTable::genarateTableFromMultiArray($data);
+?>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
